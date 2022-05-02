4ef4790a0b3329a776a97a40769a69efee2d96e3
Lukinhas123Ito <lucastoshihiroito28@gmail.com>
2022-05-01 22:22:01 -0300

    Commit

--- Model/connect.php
+++ Model/connect.php
@@ -1,9 +1,21 @@
 <?php
 
-$conn = new mysqli('loc');
+$name ="localhost";
+$dbname = "test";
+$user ="root";
+$pass ="bil0u!@#";
 
-if($conn -> connect_error){
-    echo "deu pau";
+
+
+
+try{
+    $conn = new PDO("mysql:host=$name;dbname=$dbname",$user,$pass );
+
+    $conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Deu certo";
+
+}catch(PDOException $e){
+    echo "Deu pau" . $e->getMessage();
 }
 
 ?>
