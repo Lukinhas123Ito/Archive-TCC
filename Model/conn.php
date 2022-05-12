2280dc81d3a377d335bb8776236bc0e60e8201a6
Lukinhas123Ito <lucastoshihiroito28@gmail.com>
2022-05-12 11:04:38 -0300

    Commit

--- Model/conn.php
+++ Model/conn.php
@@ -1,11 +1,12 @@
 <?php
 
+include('../.vscode/sftp.json');
 //Brige to PHP to Mysql
 
-define('HOST', 'localhost');
-define('user', 'root');
-define('senha', 'bil0u!@#');
-define('db', 'teste');
+define('HOST', 'sql106.epizy.com');
+define('user', 'epiz_31710289');
+define('senha', 'apr2aCzwe82');
+define('db', 'epiz_31710289');
 
 
 $name ="localhost";
